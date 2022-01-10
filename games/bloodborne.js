494add58e0113214ab824fba3f6e69c51ce7b449
Dennis Campos <dennisjr89@gmail.com>
2022-01-09 20:24:06 -0500

    added bloodborne to the api section

---
+++ games/bloodborne.js
@@ -0,0 +1,9 @@
+const bloodBorne = [
+  {
+    name: "Bloodborne",
+    baseUrl: "https://bloodborne.wiki.fextralife.com",
+    bossUrl: "https://bloodborne.wiki.fextralife.com/Bosses",
+  },
+];
+
+module.exports = bloodBorne;
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const darkSouls1 = require("../games/darksouls1");
 const darkSouls2 = require("../games/darksouls2");
 const demonsSouls = require("../games/demonssouls");
+const bloodBorne = require("../games/bloodborne");
 
 router.get("/darksouls1", (req, res) => {
   const games = [];
@@ -49,6 +50,7 @@ router.get("/darksouls2", (req, res) => {
           const url = $(this).attr("href");
           const bossImage = $(this).find("img").attr("src");
           games.push({
+            game: game.name,
             boss: bossName,
             url: `${game.baseUrl}${url}`,
             image: `${game.baseUrl}${bossImage}`,
@@ -75,8 +77,35 @@ router.get("/demonssouls", (req, res) => {
           const bossName = $(this).text().replace(/\n/g, "").trim();
           const url = $(".special .wiki_link", this).attr("href");
           const bossImage = $(this).find("img").attr("src");
-          console.log("hey", bossImage);
+
           games.push({
+            game: game.name,
+            boss: bossName,
+            url: `${game.baseUrl}${url}`,
+            image: `${game.baseUrl}${bossImage}`,
+          });
+        });
+        res.json(games);
+      })
+      .catch((error) => console.log("Something went wrong: ", error));
+  });
+});
+
+router.get("/bloodborne", (req, res) => {
+  const games = [];
+  bloodBorne.forEach((game) => {
+    axios
+      .get(game.bossUrl)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        $(".col-sm-4").each(function () {
+          const bossName = $("h3 .wiki_link", this).text();
+          const url = $("a", this).attr("href");
+          const bossImage = $(this).find("img").attr("src");
+          games.push({
+            game: game.name,
             boss: bossName,
             url: `${game.baseUrl}${url}`,
             image: `${game.baseUrl}${bossImage}`,
