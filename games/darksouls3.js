8ae0a98bfe948b33d0bfe4b59187c7456a3ca251
Dennis Campos <dennisjr89@gmail.com>
2022-01-10 22:29:55 -0500

    added dark souls 3

---
+++ games/darksouls3.js
@@ -0,0 +1,9 @@
+const darkSouls3 = [
+  {
+    name: "Dark Souls 3",
+    baseUrl: "https://darksouls3.wiki.fextralife.com",
+    bossUrl: "https://darksouls3.wiki.fextralife.com/Bosses",
+  },
+];
+
+module.exports = darkSouls3;
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const darkSouls1 = require("../games/darksouls1");
 const darkSouls2 = require("../games/darksouls2");
 const demonsSouls = require("../games/demonssouls");
 const bloodBorne = require("../games/bloodborne");
+const darkSouls3 = require("../games/darksouls3");
 
 router.get("/darksouls1", (req, res) => {
   const games = [];
@@ -103,6 +104,33 @@ router.get("/bloodborne", (req, res) => {
         $(".col-sm-4").each(function () {
           const bossName = $("h3 .wiki_link", this).text();
           const url = $("a", this).attr("href");
+          const bossImage = $(this).find("img").attr("src");
+          games.push({
+            game: game.name,
+            boss: bossName,
+            url: `${game.baseUrl}${url}`,
+            image: `${game.baseUrl}${bossImage}`,
+          });
+        });
+        res.json(games);
+      })
+      .catch((error) => console.log("Something went wrong: ", error));
+  });
+});
+
+router.get("/darksouls3", (req, res) => {
+  const games = [];
+
+  darkSouls3.forEach((game) => {
+    axios
+      .get(game.bossUrl)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        $(".wiki_tooltip", html).each(function () {
+          const bossName = $(this).text();
+          const url = $(this).attr("href");
           const bossImage = $(this).find("img").attr("src");
           games.push({
             game: game.name,
